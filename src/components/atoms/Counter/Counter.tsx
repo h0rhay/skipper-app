f0451c3bbe8cfdd21d95e1adeb352dd070c842ef
George Clark <george.clark@holidayextras.com>
2026-03-17 16:53:38 +0000

    feat: add Counter atom

---
+++ src/components/atoms/Counter/Counter.tsx
@@ -0,0 +1,6 @@
+import styles from './Counter.module.css'
+
+interface CounterProps { current: number; total: number; prefix?: string }
+export function Counter({ current, total, prefix = 'Q' }: CounterProps) {
+  return <span className={styles.counter}>{prefix} {current} of {total}</span>
+}
